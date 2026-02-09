5706a6ff04d688fc7b4ea713436a1c3e538bc0e1
Cad3nCat <caden.yahiro@gmail.com>
2026-02-09 22:58:10 +0000

    finished tash me click working on tash me twirl

--- lessons/10_Turtles/70_Projects/30_Tash_Me_Click.py
+++ lessons/10_Turtles/70_Projects/30_Tash_Me_Click.py
@@ -6,3 +6,79 @@
 # Hint: See 08a_More Turtle Programs, section 'Click on the Screen'
  
 ... # Your code here
+import turtle as turtle
+""" 
+Tash Me
+
+Write a program that:
+1) Loads an emoji image as the background
+2) Make the turtle shape a moustache
+3) Move the moustache to the right spot on the emoji
+
+Hint: See 08a_More Turtle Programs, section 'Change the Background Image' and
+'Change the Turtle Shape'
+"""
+
+
+
+
+import turtle
+
+def set_background_image(window, image_name):
+    """Set the background image of the turtle window to the image with the given name."""
+
+    from pathlib import Path
+    from PIL import Image
+
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+
+    image = Image.open(image_path)
+    
+    window.setup(image.width, image.height, startx=0, starty=0)
+    window.bgpic(image_path)
+
+# Set up the screen
+import turtle                           # Tell Python we want to work with the turtle
+turtle.setup(width=700, height=700)     # Set the size of the window
+
+tina = turtle.Turtle()                  # Create a turtle named tina
+
+screen = turtle.Screen()                # Get the screen that tina is on
+set_background_image(screen, "emoji2.png")
+def set_turtle_image(turtle, image_name):
+    """Set the turtle's shape to a custom image."""
+
+    from pathlib import Path
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+
+    screen = turtle.getscreen()
+    screen.addshape(image_path)
+    turtle.shape(image_path)
+
+# Set up the screen
+screen = turtle.Screen()
+screen.setup(width=600, height=600)
+
+# Create a turtle and set its shape to the custom GIF
+
+
+set_turtle_image(tina, "moustache2.gif")
+tina.shapesize(1,1)
+
+tina.penup
+
+
+# This is the function that gets called when you click on the screen
+def screen_clicked(x, y):
+    """Print the x and y coordinates of the screen when clicked.
+    and make the turtle move to the clicked location."""
+
+    print('You pressed: x=' + str(x) + ', y=' + str(y))
+
+    tina.goto(x, y)
+  
+screen.onclick(screen_clicked) # Important! Tell Python which function to use when the screen is clicked
+
+turtle.done() # Important! Use `done` not `exitonclick` to keep the window open
