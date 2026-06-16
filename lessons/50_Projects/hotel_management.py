c0bdd2b19a3b32f82953315100f8f26b3d13ed5d
Cad3nCat <caden.yahiro@gmail.com>
2026-06-16 00:00:46 +0000

    STILL ON HOTEL MANAGEMENT!!!!!!! UGHHHHHH!!!!!!!!!

--- lessons/50_Projects/hotel_management.py
+++ lessons/50_Projects/hotel_management.py
@@ -1,22 +1,33 @@
-roomAvalibility = [True, True, True, True, True, True, True, True, True, True]
+guests = []
+rooms = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+avalibility = {}
+
 
 def what_to_do():
     answer = input("what do you want to do? (1) Check into your room?")
     if answer == "1":
         return 1
 
-
-
 def find_Avalible_Room():
-    for i in range(len(roomAvalibility)):
-        if roomAvalibility[i] == True:
-            return i 
+    guest_name = ask_res_name()
+    for i in len(avalibility):
+        if avalibility[i] != rooms[i] and guests[i]
+    
+def ask_res_name():
+    name = input("what's your name?")
+    return name
+
+def ask_res_nights():
+    night = int(input("how many nights do you want to stay?"))
+    return night
+
+nights = ask_res_nights()
+
+def price_for_res():
+    price = nights * 100
+    return price
 
 
-
-def ask_res_info():
-    name = input("whats your name?")
-    nights = int(input("how many nights do you want to stay?"))
 whatToDoAnswer = what_to_do()
 
 
